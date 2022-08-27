3038beb52ac454851d77c725c3893b5045beb3d2
Fakea Vangchhia <75936459+FakeaVangchhia@users.noreply.github.com>
2022-08-27 14:45:21 +0530

    Delete ClassWeb directory

--- ClassWeb/coreSite/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoresiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coreSite'
--- ClassWeb/coreSite/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def hi(request):
-    return render(request, 'coreSite/home.html')
